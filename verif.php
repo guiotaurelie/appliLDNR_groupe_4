d2981c015d705eca3c85ffb7d8d581d5b6c63f66
Bounasmir Elias <elias.bounasmir@gmail.com>
2019-05-28 20:43:44 +0200

    Correct_Nom_Var+ Harmoni_Code+Test

--- verif.php
+++ verif.php
@@ -1,3 +1,4 @@
+
 <!-- Vérification du contenu d'un formulaire -->
 <!DOCTYPE html>
 <html lang="fr">
@@ -7,16 +8,48 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>Vérification du contenu d'un formulaire</title>
-<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="main.css">
+
 </head>
 
 <body id="top">
 <h1>Vérification du contenu d'un formulaire</h1>
-<p>Contenu de $_GET, $_POST et $_COOKIE</p>
-<strong>Vous devez exécuter ce programme dans un serveur Web acceptant PHP pour voir une réponse !</strong>
+
+
 <pre>
+<!-- Vérification du contenu d'un formulaire -->
 <?php
-print_r($_REQUEST);
+include 'valid.php';
+
+
+echo Verif();
+echo Affichage();
+
+function Verif()
+{
+  $nom = $_POST['nom'];
+  $prenom = $_POST['prenom'];
+  $date = $_POST['date_de_naissance'];
+  $titre = $_POST['titre'];
+  //vérification de champ rempli Champ par champ
+  if (($nom == '') || ($nom == 'Mon nom') )
+  {
+    echo '<h2>veuillez entrer votre nom</h2>';
+    echo'<br/>';
+  } 
+  if (($prenom == '') || ($prenom == 'Mon Prénom')) 
+  {
+    echo '<hr>';
+    echo '<h2>veuillez entrer votre prénom</h2>';
+  } 
+  if ($date == '' ) 
+  {
+    echo '<h2>veuillez entrer votre date de naissance</h2>';
+  } 
+}
+
+
+
 ?>
 </pre>
 <p>Retourner à la page d'<a href="index.html" title="Retourner au formulaire">accueil</a></p>
